d36fd9264202e4cbc0f7d6b6fafb53c969c45e74
henriquechinglong <henriquechinglong@gmail.com>
2025-09-11 01:02:38 +0000

    Verificando se o tailwind está funcionando

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">
+        TailwindCSS está funcionando 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
