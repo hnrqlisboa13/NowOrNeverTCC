5f609151e6e13ddd81fd4e2fc92dbc5b939479ca
henriquechinglong <henriquechinglong@gmail.com>
2025-10-30 15:35:33 +0000

    atualização

--- src/components/Instrucoes.jsx
+++ src/components/Instrucoes.jsx
@@ -132,7 +132,7 @@ function Instrucoes() {
                                 <div className="relative w-full pb-[58.25%]"> 
                                     <iframe
                                         className="absolute top-0 left-0 w-full h-full"
-                                        src="https://www.youtube.com/embed/sSIZ34D9qC8"
+                                        src="https://www.youtube.com/embed/sYjQC_PmmbE"
                                         title="YouTube video player"
                                         frameBorder="0"
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
